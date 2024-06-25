9eb41488d8887b6400107e605189cb17609e6640
sunetra.mukherjee <sunetra.mukherjee@tcgdigital.com>
2024-06-25 20:26:13 +0530

    Updated commit -25-06

--- src/main/java/com/example/demo/DdbDeleteApplication.java
+++ src/main/java/com/example/demo/DdbDeleteApplication.java
@@ -3,6 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @SpringBootApplication
@@ -11,6 +15,16 @@ public class DdbDeleteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DdbDeleteApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+	@Bean
+	public ObjectMapper objectMapper(){
+		return new ObjectMapper();
 	}
 
 }

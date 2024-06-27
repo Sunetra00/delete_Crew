161cc07938f6737e3b3bc1efc85a80926002c58c
Parth <parth.bhatt13@outlook.com>
2024-06-27 23:16:32 +0530

    updated code

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:8
-ADD target/deleteCrew-0.0.1-SNAPSHOT.jar app.jar
+ADD target/deleteCrew-0.0.1-SNAPSHOT.jar  app1.jar
 EXPOSE 8080
-ENTRYPOINT ["java","-jar","app.jar"]
+ENTRYPOINT ["java","-jar","app1.jar"]
--- src/main/java/com/example/demo/service/Deleteresttemplateservice.java
+++ src/main/java/com/example/demo/service/Deleteresttemplateservice.java
@@ -3,20 +3,42 @@ package com.example.demo.service;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.example.demo.entity.Crew;
+import com.example.demo.entity.ProcessedRequestDTO;
+import com.example.demo.entity.Response;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class Deleteresttemplateservice {
 
-	RestTemplate resttemplate = new RestTemplate();
+	@Autowired
+	ObjectMapper mapper;
+
+	@Autowired
+	RestTemplate restTemplate;
 	final String URL = "http://dynamodbupdater/ddboperation";
 
-	public void deleteCrew(String id) {
-		Map<String, String> param = new HashMap<String, String>();
-		param.put("crewid", id);
-		resttemplate.delete(URL, param);
+	public void deleteCrew(String id) throws JsonProcessingException {
+		ProcessedRequestDTO requestDTO = deleteRequest(id);
+		String jsonRequest = mapper.writeValueAsString(requestDTO);
+		System.out.println(jsonRequest);
+		HttpEntity request = new HttpEntity<>(requestDTO);
+		ResponseEntity<Response> response = restTemplate.exchange(URL, HttpMethod.POST,request, Response.class);
+	}
 
+	private ProcessedRequestDTO deleteRequest(String id) {
+		ProcessedRequestDTO
+				requestDTO = new ProcessedRequestDTO();
+		requestDTO.setCrewid(id);
+		requestDTO.setOperationType("DELETE");
+		return requestDTO;
 	}
 
 }
